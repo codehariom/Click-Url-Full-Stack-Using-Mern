f19a0be0d51ef170c2b2dc6841a7316874a6500b
realhariom <digitalwithhariom@gmail.com>
2025-05-27 17:35:39 +0530

    Update profileControllers.js

--- backend/controllers/profileControllers.js
+++ backend/controllers/profileControllers.js
@@ -54,14 +54,11 @@ export const userProfile = expressAsyncHandler(async (req, res) => {
 
 
 export const getUserPic = expressAsyncHandler(async (req, res) => {
-  const { username } = req.query; // Use query or params for GET request
-
-  if (!username) {
-    return res.status(400).json({ message: "Username is required" });
-  }
-
   try {
-    const user = await User.findOne({ username });
+    // Get user info from request (set by authenticateUser middleware)
+    const userId = req.user._id;
+
+    const user = await User.findById(userId);
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
